cc6654d976e9b8ba7ee818b4b31003cb21da9602
masterhamzaa <master.hamza4576@gmail.com>
2023-09-12 12:41:45 +0100

    vercel up-1

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const serverless = require('serverless-http');
 const server = express();
 server.use(express.json());
-server.use(cors({ origin: "http://localhost:3000", methods: ["POST", "GET"], credentials: true }));
+//server.use(cors({ origin: "http://localhost:3000", methods: ["POST", "GET"], credentials: true }));
 
 // mongo database
 const { mdb } = require("./database/db");
@@ -14,7 +14,11 @@ mdb();
 const routes = require("./Routes/Router");
 server.use("/", routes);
 
-// server listen
+
+module.exports.handler = serverless(server);
+
+/* // server listen
 server.listen(process.env.port, () => {
   console.log("server running with success...");
 });
+ */
