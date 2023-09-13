50984a132ba99afa2025efe483007a5d2bac1ffc
masterhamzaa <master.hamza4576@gmail.com>
2023-09-13 12:17:27 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const serverless = require('serverless-http');
+
 const server = express();
 server.use(express.json());
-//server.use(cors({ origin: "http://localhost:3000", methods: ["POST", "GET"], credentials: true }));
-
+//server.use(cors({origin: "http://localhost:3000"}));
 
 // mongo database
 const { mdb } = require("./database/db");
@@ -13,13 +12,9 @@ mdb();
 
 // routes
 const routes = require("./Routes/Router");
-server.use("/", routes);
+server.use("/",routes);
 
-
-module.exports.handler = serverless(server);
-
-/* // server listen
+// server listen
 server.listen(process.env.port, () => {
   console.log("server running with success...");
 });
- */
