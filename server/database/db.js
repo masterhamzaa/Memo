9483540f731762962b68e2e823ab5632ea12f393
masterhamzaa <master.hamza4576@gmail.com>
2023-09-13 12:56:32 +0100

    update

--- server/database/db.js
+++ server/database/db.js
@@ -1,18 +1,12 @@
 const mdb = async () => {
     require("dotenv").config()
     const mongoose = require("mongoose")
-    mongoose.set("strictQuery", false)
-    await mongoose.connect(process.env.mdb, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    const cluster = mongoose.connection;
-    cluster.once("open", () => {
-        console.log("Express Cluster ready for operations....");
-    })
-    cluster.on("error", (err) => {
-        console.log("error : " + err);
-    })
+    try {
+        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        console.log("Connect to MongoDB successfully")
+    } catch (error) {
+        console.log("Connect failed " + error.message )
+    }
 }
 
 module.exports = { mdb }
