af47f4287a8dcd07e8498e20eb5cc5a3a9497bcb
masterhamzaa <master.hamza4576@gmail.com>
2024-01-25 15:28:39 +0100

    bug23iu8

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -40,7 +40,8 @@ export default function Login() {
 
       if (!res.err) {
         //localStorage.setItem("token", res.token);
-        localStorage.setItem("user", data.userid);
+        localStorage.setItem("user", res.userid);
+        console.log(data.userid)
         go("/Postits")
       } else {
         Swal.fire({
